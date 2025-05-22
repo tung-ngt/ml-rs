a6c7787bc4a35a9b75b4e9b04ec2b7bcecbe8e8b
tungngt <tung.nguyent03@gmail.com>
2025-05-22 16:28:32 +0700

    add mnist classification

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-use ml_rs::examples::{simple_image_classification::train, test_read_csv::test_read_csv};
+use ml_rs::examples::mnist_classification::train;
 
 fn main() {
-    //let mut random_generator = PCG::new(32, 6);
-    //let mut random_fn = || random_generator.next_normal(0.0, 0.05);
-    //logic_gate::train_sigmoid(
-    //    logic_gate::xor_dataset(),
-    //    true,
-    //    10 * 1000,
-    //    10.0,
-    //    Some(&mut random_fn),
-    //);
-
-    //train();
-
-    test_read_csv();
+    train();
 }
