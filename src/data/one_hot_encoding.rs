7f778b095761d14d7f6c1f469861287eef338200
tungngt <tung.nguyent03@gmail.com>
2025-05-31 17:56:55 +0700

    add assert to onehot

--- src/data/one_hot_encoding.rs
+++ src/data/one_hot_encoding.rs
@@ -2,13 +2,20 @@ use crate::tensor::Tensor;
 
 pub fn one_hot_encoding(labels: Tensor<1>, no_classes: usize) -> Tensor<2> {
     let batch = labels.no_elements();
-    let mut data = vec![0.0; batch * no_classes];
+    let mut data = vec![0.01; batch * no_classes];
     let shape = [batch, no_classes];
     let strides = Tensor::get_strides(&shape);
 
     for b in 0..batch {
         let label = labels[b] as usize;
-        data[b * strides[0] + label] = 1.0;
+        assert!(
+            label < no_classes,
+            "row {} has label {} > no classes {}",
+            b,
+            label,
+            no_classes
+        );
+        data[b * strides[0] + label] = 0.91;
     }
 
     Tensor::with_data(&shape, &strides, 0, data.into())
